806e4a667f0877aa6d2e6f9e4a75bf00fabec288
codingma <454226880@qq.com>
2017-04-01 16:03:24 +0800

    开始编写分类服务相关代码

--- run/main_classifier.py
+++ run/main_classifier.py
@@ -299,23 +299,26 @@ class MainClassifier(object):
 if __name__ == '__main__':
     # 训练和评测阶段，这里把所有可能需要自定义的参数全部都移到配置文件里了，如果需要也可以换成传参调用的形式
     # 需要外面传进来的参数只有训练集的位置和验证集的位置
-    mainClassifier = MainClassifier()
-    Util.log_tool.log.debug("lexicon locked:" + str(mainClassifier.lexicon.locked))
-    # # 根据原始语料进行语料预处理（切词、过滤、特征降维）
-    mainClassifier.construct_lexicon(FilePathConfig.total_corpus_path)
-    # # 训练
-    mainClassifier.train(FilePathConfig.train_corpus_path)
-    # # 测试
-    mainClassifier.test(FilePathConfig.test_corpus_path)
+    # mainClassifier = MainClassifier()
+    # Util.log_tool.log.debug("lexicon locked:" + str(mainClassifier.lexicon.locked))
+    # # # 根据原始语料进行语料预处理（切词、过滤、特征降维）
+    # mainClassifier.construct_lexicon(FilePathConfig.total_corpus_path)
+    # # # 训练
+    # mainClassifier.train(FilePathConfig.train_corpus_path)
+    # # # 测试
+    # mainClassifier.test(FilePathConfig.test_corpus_path)
     # ----------------------------------------------------------------------------------------------------
     # 对外来的数据进行分类
-    # mainClassifier = MainClassifier()
-    # mainClassifier.load_model()
-    # # 需要分类的数据
-    # raw_document = "{json}"
-    # # 只返回单分类
-    # classify_result = mainClassifier.classify_document(raw_document)
-    # # 需要返回的类别数量
-    # k = 3
-    # # 返回多个分类和其概率
-    # classify_results = mainClassifier.classify_document_top_k(raw_document, k)
+    mainClassifier = MainClassifier()
+    mainClassifier.set_model()
+    # 需要分类的数据
+    raw_document = '{"ID":"110","url":"http://news.ifeng.com/a/20150330/43444856_0.shtml","title":"柯文哲纠正司仪“恭请市长”说法：别用封建语言","splitTitle":"柯文哲_nr 纠正_v 司仪_n 恭请_v 市长_x 说法_k ：_w 别_k 用_k 封建_k 语言_n ","splitContent":"  _w 原_k 标题_n ：_w 纠正_v 恭请_v  _w 柯文哲_nr ：_w 别_k 用_k 封建_k 语言_n  _w \n_w  _w \n_w  _w 台海网_n 3月_t 30日_t 讯_k  _w 据_h 中国时报_x 报道_k ，_w 台北市府_x 29日_mq 举办_v 首长_n 领航_k 营_k ，_w 司仪_n 刚_d 宣布_v 恭请_v 市长_x 致词_k 后_f ，_w 旋_k 遭_k 柯文哲_nr 打_k 脸_n 不要_v 再_d 用_k 这种_mq 封建_k 时代_n 的_u 语言_n ，_w 引起_v 现场_n 哄堂大笑_i 。_w 但_k 卫生局_x 在_p 讲义_n 中_n 整理_v 40条_mq 柯_k 语录_x ，_w 连_h 奇怪_a 耶_k ，_w 听_k 起来_v 怪怪的_z 都_n 入列_v ，_w 卫生局长_x 黄世杰_nr 澄清_k ，_w 市长_x 的_u 核心_n 价值_n 很_d 重要_a ，_w 才_h 会_v 整理_v 市长_x 说_v 过_v 的_u 重点_h ，_w 纯属_v 参考_v 不用_v 牢记_v 或_d 盲从_v 。_w  _w \n_w  _w \n_w  _w 柯文哲_nr 透露_v ，_w 有_v 次_q 去_n 参加_v 国民党_x 一个_mq 会议_n ，_w 他们_r 很_d 喜欢_v 互_d 称_v 什么_r 公_k 、_w 什么_r 公_k ，_w 像_k 吴伯雄_x 就_d 被_p 称为_v 伯公_n 。_w 因此_c 有_v 次_q 遇到_v 民进党_x 主席_x 蔡英文_nr ，_w 自己_h 就_d 和_c 她_r 提起_v 这件_mq 事_k ，_w 并_h 说_v 还好_v 民进党_x 没有_v 这种_mq 文化_x ，_w 不然_c 像_k 苏贞昌_x 不_h 就_d 变_v 冲_k 公_k （_w 台语_nz ）_w ？_w  _w \n_w  _w \n_w  _w 柯文哲_nr 分享_v 完_h 这段_mq 故事_n 后_f 不_h 忘_v 再次_d 提醒_v 司仪_n ，_w 以后_f 就_d 不要_v 再_d 用_v 恭请_v 这种_mq 封建_k 时代_n 的_u 语言_n 。_w ","publishedTime":"2015-03-30 09:24:00","source":"台海网","appId":"recommend","docType":"doc","other":"source\u003dspider|!|channel\u003dnews|!|tags\u003d凤凰网资讯-台湾-台湾时政|!|imgNum\u003d0","features":["台湾","c","1.0","时政","c","-1.0","台湾时政","cn","1.0","国民党","et","0.1","苏贞昌","et","0.1","民进党","et","0.1","蔡英文","et","0.1","柯文哲","et","1.0","台北市府","et","0.1","吴伯雄","et","0.1","市长","x","-0.5","中国时报","x","-0.1","卫生局长","x","-0.1","卫生局","x","-0.1","黄世杰","nr","-0.1","台语","nz","-0.1","讲义","n","-0.1","台海网","n","-0.1","时代","n","-0.1","司仪","n","-0.5","语言","n","-0.5","伯公","n","-0.1","首长","n","-0.1"]}	标题,纠正,恭请,柯文哲,语言,台海网,中国时报,台北市府,举办,首长,司仪,恭请,市长,柯文哲,语言,哄堂大笑,卫生局,讲义,整理,语录,入列,卫生局长,黄世杰,市长,核心,价值,整理,市长,纯属,参考,牢记,盲从,柯文哲,透露,参加,国民党,会议,喜欢,吴伯雄,称为,伯公,民进党,主席,蔡英文,提起,还好,民进党,文化,苏贞昌,台语,柯文哲,分享,故事,提醒,司仪,恭请,语言	原标题：纠正恭请柯文哲：别用封建语言台海网3月30日讯据中国时报报道，台北市府29日举办首长领航营，司仪刚宣布恭请市长致词后，旋遭柯文哲打脸不要再用这种封建时代的语言，引起现场哄堂大笑。但卫生局在讲义中整理40条柯语录，连奇怪耶，听起来怪怪的都入列，卫生局长黄世杰澄清，市长的核心价值很重要，才会整理市长说过的重点，纯属参考不用牢记或盲从。柯文哲透露，有次去参加国民党一个会议，他们很喜欢互称什么公、什么公，像吴伯雄就被称为伯公。因此有次遇到民进党主席蔡英文，自己就和她提起这件事，并说还好民进党没有这种文化，不然像苏贞昌不就变冲公（台语）？柯文哲分享完这段故事后不忘再次提醒司仪，以后就不要再用恭请这种封建时代的语言。	时政'
+    feature_mat = mainClassifier.corpus_to_feature_mat(raw_document)
+    # 只返回单分类
+    classify_result = mainClassifier.classify_document(feature_mat)
+    print classify_result
+    # 需要返回的类别数量
+    k = 3
+    # 返回多个分类和其概率
+    classify_results = mainClassifier.classify_documents_top_k(raw_document, k)
+    print classify_results
