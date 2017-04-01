c32032d2fb617e54b041bbe1bbc39f884a337d18
codemayq <454226880@qq.com>
2017-04-01 17:23:57 +0800

    修改了预测top_k的逻辑

--- model/abstract_classifier.py
+++ model/abstract_classifier.py
@@ -1,7 +1,11 @@
+# coding=UTF-8
 import cPickle
+
+import numpy as np
 
 from config.config import ClassifierConfig
 from misc.util import Util
+
 class AbstractClassifier(object):
 
     def __init__(self):
@@ -9,15 +13,30 @@ class AbstractClassifier(object):
         self.model_path = None
         pass
 
-    def classify(self, document):
-        return self.classify_top_k(document, 1)
-
     def classify_top_k(self, documents, top_k):
         if self.model is None:
             self.load_model()
+        # 检查top_k
+        if top_k < 1:
+            top_k = 1
+        top_k = int(top_k)
 
-        raw_results = self.model.predict(documents)
-        return raw_results
+        final_results = []
+        # 如果当前分类器支持返回概率，则走概率
+        if ClassifierConfig.cur_single_model in ClassifierConfig.can_predict_pro_classifiers:
+            pro_of_lines = self.model.predict_proba(documents)
+            line_result = []
+            for pro_of_line in pro_of_lines:
+                # 降序获得概率最大的
+                pro_index = np.argsort(-pro_of_line)
+                for i in range(top_k):
+                    line_result.append((pro_index[i], pro_of_line[pro_index[i]]))
+            final_results.append(line_result)
+        else:
+            raw_results = self.model.predict(documents)
+            for raw_result in raw_results:
+                final_results.append((raw_result, 1))
+        return final_results
 
     def save_model(self):
         cPickle.dump(self.model, open(self.model_path, 'w'))
@@ -26,6 +45,8 @@ class AbstractClassifier(object):
         if not Util.is_file(self.model_path):
             Util.log_tool.log.error("model not exist")
             Util.quit()
+        else:
+            Util.log_tool.log.debug("loading model")
         self.model = cPickle.load(open(self.model_path, 'r'))
 
     def train(self, feature_mat, label_vec):
